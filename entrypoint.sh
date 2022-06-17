aafca26766c9dcc9004790eb2cd2c0ed1226b3fd
Yaseen Hamdulay <yaseen@stitch.money>
2022-06-17 14:36:23 +0200

    add correct schema location

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,16 @@ run_kubeval() {
     # Validate all generated manifest against Kubernetes json schema
     cd "$1"
     VALUES_FILE="$2"
-    mkdir helm-output;
+    mkdir -p helm-output;
     helm template --values "$VALUES_FILE" --output-dir helm-output .;
     find helm-output -type f -exec \
-        /kubeval/kubeval \
+        kubeval \
             "-o=$OUTPUT" \
             "--strict=$STRICT" \
             "--kubernetes-version=$KUBERNETES_VERSION" \
             "--openshift=$OPENSHIFT" \
             "--ignore-missing-schemas=$IGNORE_MISSING_SCHEMAS" \
+            "--schema-location=https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master" \
         {} +;
     rm -rf helm-output;
 }
@@ -29,24 +30,22 @@ do
     REPO=$(echo $REPO_CONFIG | cut -d '=' -f1);
     URL=$(echo $REPO_CONFIG | cut -d '=' -f2);
     helm repo add $REPO $URL;
-done < $CONFIG_FILE
+done < "$CONFIG_FILE"
 
 helm repo update
 
 # For all charts (i.e for every directory) in the directory
 for CHART in "$CHARTS_PATH"/*/; do
+    echo "Chart is $CHART"
     echo "Validating $CHART Helm Chart...";
-    cd "$CURRENT_DIR/$CHART";
+    cd "$CHART";
     helm dependency build --skip-refresh;
 
-    if [ -d "ci" ]; then
-        for VALUES_FILE in *-values.yaml; do
-            echo "Validating $CHART Helm Chart using $VALUES_FILE values file...";
-            run_kubeval "$(pwd)" "$VALUES_FILE"
-        done
-    else
-        run_kubeval "$(pwd)" "/dev/null"
-    fi
+    for VALUES_FILE in *-values.yaml; do
+        echo "Validating $CHART Helm Chart using $VALUES_FILE values file...";
+        run_kubeval "$(pwd)" "$VALUES_FILE"
+    done
+    run_kubeval "$(pwd)" "values.yaml"
     echo "Cleanup $(pwd)/charts directory after we are done running Kubeval"
     rm -rf $(pwd)/charts/
 done
